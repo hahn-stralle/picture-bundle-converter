8cfc708a1f9540a7453b88d117105500bb73a8c0
Hahn Stralle <84215792+hahn-stralle@users.noreply.github.com>
2021-05-23 19:38:02 +0900

    Add files via upload

--- convert_png.cmd
+++ convert_png.cmd
@@ -16,7 +16,7 @@ if not %1==return (
 ) else (
 	rem 入力画像ファイルをpngに変換して出力します。
 	mkdir %export_directory_path% 2>nul
-	magick -monitor %imd%compression-level=9 %imd%compression-filter=9 -quality 99 %2 png32:%export_directory_path%%~n2%export_file_extension%
+	magick -monitor %imd%compression-level=9 -quality 99 %2 png32:%export_directory_path%%~n2%export_file_extension%
 )
 endlocal
 exit /b
